38c7ab828659605b9e91cc86618a9e0035f35def
VanesaGR <vanesaglezrguez@gmail.com>
2023-02-24 13:38:48 +0100

    Render cocktails, search & reset button

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const input = document.querySelector('.js-input');
 const btnSearch  = document.querySelector('.js-search-btn');
 const btnReset = document.querySelector('.js-reset-btn');
 const listCocktails = document.querySelector('.js-cocktails');
-const favoritesUl = document.querySelector('.js-favorites');
+const listFavorites = document.querySelector('.js-favorites');
 
 const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
 
@@ -26,6 +26,7 @@ function renderCocktails(cocktails) {
 ./assets/images/default.png" title="${eachCocktail.name}" class="imgCocktail"/></li>`; //si no tiene foto te pone la seleccionada por defecto
     }
   }
+  addEventToCocktail();
 }
 
 function fetchFunction(cocktailName){
@@ -50,10 +51,68 @@ function handleClickBtn(ev) {
   fetchFunction(inputValue);//aprovechamos la funcion anterior
 }
 
+//funcion para el boton de reset
+function handleResetBtn(ev){
+  ev.preventDefault();
+  input.value='';
+}
 
+//función para pintar favoritos (es igual que la de por defecto solo que cambia el array que la recibe)
+function renderFavorites(cocktails) {
+  listCocktails.innerHTML='';
+  for (const eachCocktail of cocktails) {
+    if (eachCocktail.photo) {
+      listFavorites.innerHTML += `<liclass="js-li-cocktail" id="${eachCocktail.idDrink}"><h3>${eachCocktail.name}</h3> <img src="${eachCocktail.photo}" title="${eachCocktail.name}" class="imgCocktail"/></li>`;
+    } else {
+      listFavorites.innerHTML += `<li class="js-li-cocktail" id="${eachCocktail.idDrink}"><h3>${eachCocktail.name}</h3> <img src="
+./assets/images/default.png" title="${eachCocktail.name}" class="imgCocktail"/></li>`; //si no tiene foto te pone la seleccionada por defecto
+    }
+  }
+  addEventToCocktail();
+}
 
+//almacenamiento en el local storage
+const cocktailStorage=JSON.parse(localStorage.getItem('listCocktails'));
+if(cocktailStorage!==null){
+  listFavoritesData = cocktailStorage;
+  renderFavorites(listFavoritesData);
+}
+
+//guardar los favoritos
+function handleClick(ev){
+
+  ev.currentTarget.classList.toggle('selected');//si tiene esa clase, se la quitas y si no, se la pones
+  //busca ese id en el listado de cocteles el coctel que tiene el id del current Target
+  const idSelected = ev.currentTarget.id;
+  //find devuelve el primer elemento que cumple una condición
+  const selectedCocktail = listCocktailsData.find(cocktail=> cocktail.id===idSelected);
+
+  //findIndex devuelve la posición donde está un elemento o -1 si es que no está
+  const indexCocktail = listFavoritesData.findIndex(eachCocktail=> eachCocktail.id===idSelected);
+
+  //comprobación de existencia del favorito
+  if(indexCocktail===-1){
+    listFavoritesData.push(selectedCocktail); //guardado en el listado con push
+  }else{
+    listFavoritesData.splice(indexCocktail, 1); //elimina un elemento a partir de una posicion
+  }
+
+  renderFavorites(listFavoritesData); //pinta favoritos
+  renderCocktails(listCocktailsData);
+}
+
+//funcion para que funcione el clic de favoritos
+
+function addEventToCocktail(){
+  const liElemenstList = document.querySelectorAll('.js-li-cocktail'); //todos los elementos con esa clase
+  for(const li of liElemenstList){//para todos los elementos de la lista
+    li.addEventListener('click', handleClick);
+  }
+}
 
 //EVENTOS
 
 //escucha del botón de búsqueda
 btnSearch.addEventListener('click', handleClickBtn);
+//escucha del botón reset
+btnReset.addEventListener('click', handleResetBtn);
